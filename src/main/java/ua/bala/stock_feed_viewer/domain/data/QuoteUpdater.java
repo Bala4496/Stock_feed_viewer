a7e98ebde0f3126376ab3a54032c0f6e9042a852
Bala4496 <igor.balabay01@gmail.com>
2023-09-17 01:00:50 +0300

    Updating QuoteUpdater

--- src/main/java/ua/bala/stock_feed_viewer/domain/data/QuoteUpdater.java
+++ src/main/java/ua/bala/stock_feed_viewer/domain/data/QuoteUpdater.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import ua.bala.stock_feed_viewer.domain.model.Company;
 import ua.bala.stock_feed_viewer.domain.model.Quote;
 import ua.bala.stock_feed_viewer.domain.repository.CompanyRepository;
 import ua.bala.stock_feed_viewer.domain.repository.QuoteRepository;
@@ -12,25 +13,44 @@ import ua.bala.stock_feed_viewer.ports.in.QuoteFetcher;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class QuoteUpdater implements DataUpdater {
 
+    private static final int BATCH_SIZE = 100;
     private final CompanyRepository companyRepository;
     private final QuoteFetcher quoteFetcher;
     private final QuoteRepository quoteRepository;
+    private ExecutorService executorService;
 
     @EventListener(ApplicationReadyEvent.class)
     @Override
     public void update() {
+        executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
         var thread = new Thread(new UpdateQuotesTask(), "UpdateQuotes");
         thread.setDaemon(true);
         thread.start();
+    }
+
+    private void shutdown() {
+        executorService.shutdown();
+
+        try {
+            if (!executorService.awaitTermination(10, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
     class UpdateQuotesTask implements Runnable {
@@ -38,80 +58,61 @@ public class QuoteUpdater implements DataUpdater {
         @Override
         public void run() {
             log.info("Thread - {} : started", Thread.currentThread().getName());
-            var batchSize = 10;
             while (true) {
-                var executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-                var companies = companyRepository.findAll();
-
-                try {
-                    for (int i = 0; i < companies.size(); i += batchSize) {
-                        var batch = companies.subList(i, Math.min(i + batchSize, companies.size()));
-
-                        var futures = batch.stream()
-                                .map(company -> CompletableFuture.runAsync(() -> {
-                                    var companyCode = company.getCode();
-                                    var savedQuote = quoteRepository.findLatestQuoteByCompanyCode(companyCode).orElse(null);
-                                    var fetchedQuote = quoteFetcher.fetchQuoteByCompanyCode(companyCode).orElse(null);
-
-                                    if (Objects.nonNull(fetchedQuote) && !fetchedQuote.equals(savedQuote)) {
-                                        fetchedQuote.setGapPercentage(calculateGapPercentage(savedQuote, fetchedQuote));
-                                        quoteRepository.save(fetchedQuote);
-                                    }
-                                }, executorService))
-                                .toList();
-
-                        var allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-
-                        try {
-                            allOf.get();
-                        } catch (Exception e) {
-                            log.error("Error while updating quotes", e);
-                        }
-                    }
-                } finally {
-                    executorService.shutdown();
-                }
-
-//                var futures = all.stream()
-//                        .map(company -> CompletableFuture.runAsync(() -> {
-//                            String companyCode = company.getCode();
-//                            var savedQuoteFuture = CompletableFuture.supplyAsync(() -> quoteRepository.findLatestQuoteByCompanyCode(companyCode).orElse(null), executorService);
-//                            var fetchedQuoteFuture = CompletableFuture.supplyAsync(() -> quoteFetcher.fetchQuoteByCompanyCode(companyCode).orElse(null), executorService);
-//
-//                            var combinedFuture = savedQuoteFuture.thenCombineAsync(
-//                                    fetchedQuoteFuture,
-//                                    (savedQuote, fetchedQuote) -> {
-//                                        if (Objects.nonNull(fetchedQuote) && !fetchedQuote.equals(savedQuote)) {
-//                                            fetchedQuote.setGapPercentage(calculateGapPercentage(savedQuote, fetchedQuote));
-//                                            return quoteRepository.save(fetchedQuote);
-//                                        }
-//                                        return savedQuote;
-//                                    }, executorService
-//                            );
-//
-//                            combinedFuture.join();
-//                        }, executorService))
-//                        .toList();
-//
-//                try {
-//                    CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get();
-//                } catch (Exception e) {
-//                    log.error("Error while updating quotes", e);
-//                } finally {
-//                    executorService.shutdown();
-//                }
+                processCompaniesInBatches(companyRepository.findAll(), BATCH_SIZE);
             }
         }
+    }
 
-        private BigDecimal calculateGapPercentage(Quote savedQuote, Quote fetchedQuote) {
-            var fetchedQuotePrice = fetchedQuote.getPrice();
-            if (Objects.isNull(savedQuote)) {
-                return fetchedQuotePrice;
+    private void processCompaniesInBatches(List<Company> companies, int batchSize) {
+        for (int i = 0; i < companies.size(); i += batchSize) {
+            List<Company> batch = companies.subList(i, Math.min(i + batchSize, companies.size()));
+
+            List<CompletableFuture<Void>> futures = batch.stream()
+                    .map(this::processCompanyAsync)
+                    .toList();
+
+            CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+
+            try {
+                allOf.join();
+            } catch (Exception e) {
+                log.error("Error while updating quotes", e);
             }
-            var savedQuotePrice = savedQuote.getPrice();
-            return fetchedQuotePrice.subtract(savedQuotePrice)
-                    .divide(savedQuotePrice, 4, RoundingMode.HALF_UP)
-                    .multiply(new BigDecimal(100));
         }
+    }
+
+    private CompletableFuture<Void> processCompanyAsync(Company company) {
+        return CompletableFuture.runAsync(() -> {
+            var companyCode = company.getCode();
+
+            var quote = quoteFetcher.fetchQuoteByCompanyCode(companyCode);
+            if (quote.isEmpty()) {
+                return;
+            }
+
+            var fetchedQuote = quote.orElse(null);
+            var savedQuote = quoteRepository.findLatestQuoteByCompanyCode(companyCode).orElse(null);
+            if (!fetchedQuote.equals(savedQuote)) {
+                fetchedQuote.setGapPercentage(calculateGapPercentage(savedQuote, fetchedQuote));
+                quoteRepository.save(fetchedQuote);
+            }
+        }, executorService);
+    }
+
+    private BigDecimal calculateGapPercentage(Quote savedQuote, Quote fetchedQuote) {
+        var fetchedQuotePrice = fetchedQuote.getPrice();
+        if (Objects.isNull(savedQuote)) {
+            return fetchedQuotePrice;
+        }
+
+        var savedQuotePrice = savedQuote.getPrice();
+        if (savedQuotePrice.compareTo(BigDecimal.ZERO) == 0) {
+            return new BigDecimal(100);
+        }
+
+        return fetchedQuotePrice.subtract(savedQuotePrice)
+                .divide(savedQuotePrice, 4, RoundingMode.HALF_UP)
+                .multiply(new BigDecimal(100));
     }
 }
